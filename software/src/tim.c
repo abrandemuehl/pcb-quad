c058aba3e61264099a9bafa288d6c30a6289c70d
Adrian Brandemuehl <abrandemuehl@gmail.com>
2017-11-27 11:47:18 -0600

    Add periodic timer commands, sleep commands, reformat

--- software/src/tim.c
+++ software/src/tim.c
@@ -3,38 +3,76 @@
 #include "stm32f4xx_gpio.h"
 #include "tim.h"
 #include "clocks.h"
+#include "uart.h"
 
 void tim_init()
 {
-	TIM_TimeBaseInitTypeDef TIM3_BaseStruct;
+  TIM_TimeBaseInitTypeDef TIM3_BaseStruct;
 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-	// Prescale from APB1 Timer Clock (APB1*2) to 500kHz
-	TIM3_BaseStruct.TIM_Prescaler = 180 - 1;
-	TIM3_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM3_BaseStruct.TIM_Period = 1000 - 1;
-	TIM3_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM3_BaseStruct.TIM_RepetitionCounter = 0;
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+  // Prescale from APB1 Timer Clock (APB1*2) to 500kHz
+  TIM3_BaseStruct.TIM_Prescaler = 180 - 1;
+  TIM3_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM3_BaseStruct.TIM_Period = 1000 - 1;
+  TIM3_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM3_BaseStruct.TIM_RepetitionCounter = 0;
 
-	TIM_TimeBaseInit(TIM3, &TIM3_BaseStruct);
-	TIM_Cmd(TIM3, ENABLE);
+  TIM_TimeBaseInit(TIM3, &TIM3_BaseStruct);
+  TIM_Cmd(TIM3, ENABLE);
 
 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	// Setup a timer to count microseconds
-	TIM_TimeBaseInitTypeDef TIM2_BaseStruct;
-	// Prescale APB1 Clock to 1 MHz
-	TIM2_BaseStruct.TIM_Prescaler = (APB1 * 2 / 1000000) - 1;
-	TIM2_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
-	// Only reload on overflow 
-	// TIM2_BaseStruct.TIM_Period = 0xFFFFFFFF;
-	// Reload every ms
-	TIM2_BaseStruct.TIM_Period = 1000-1;
-	TIM2_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM2_BaseStruct.TIM_RepetitionCounter = 0;
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+  // Setup a timer to count microseconds
+  TIM_TimeBaseInitTypeDef TIM2_BaseStruct;
+  // Prescale APB1 Clock to 1 MHz
+  TIM2_BaseStruct.TIM_Prescaler = (APB1 * 2 / 1000000) - 1;
+  TIM2_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+  // Only reload on overflow 
+  TIM2_BaseStruct.TIM_Period = 0xFFFFFFFF - 1;
+  TIM2_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM2_BaseStruct.TIM_RepetitionCounter = 0;
 
-	TIM_TimeBaseInit(TIM2, &TIM2_BaseStruct);
-	TIM_Cmd(TIM2, ENABLE);
+  TIM_TimeBaseInit(TIM2, &TIM2_BaseStruct);
+  TIM_Cmd(TIM2, ENABLE);
+
+
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+  // Setup a timer to count microseconds
+  TIM_TimeBaseInitTypeDef TIM4_BaseStruct;
+  // Prescale APB1 Clock to 1 MHz
+  TIM4_BaseStruct.TIM_Prescaler = (APB1 * 2 / 1000000) - 1;
+  TIM4_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+  // Only reload on overflow 
+  TIM4_BaseStruct.TIM_Period = 0xFFFFFFFF - 1;
+  TIM4_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM4_BaseStruct.TIM_RepetitionCounter = 0;
+
+  TIM_TimeBaseInit(TIM4, &TIM4_BaseStruct);
+  TIM_Cmd(TIM4, ENABLE);
+
+}
+
+void periodic_init(int hz) {
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+  // Setup a timer to count microseconds
+  TIM_TimeBaseInitTypeDef TIM5_BaseStruct;
+  // Prescale APB1 Clock to 1 MHz
+  TIM5_BaseStruct.TIM_Prescaler = (APB1 * 2 / 1000000) - 1;
+  TIM5_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+  // Reload based at hz rate
+  TIM5_BaseStruct.TIM_Period = (1000000 / hz) - 1;
+  TIM5_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM5_BaseStruct.TIM_RepetitionCounter = 0;
+
+  TIM_TimeBaseInit(TIM5, &TIM5_BaseStruct);
+  TIM_Cmd(TIM5, ENABLE);
+
+  TIM_ITConfig(TIM5, TIM_IT_Update, ENABLE);
+}
+
+void periodic_sleep() {
+  TIM_ClearITPendingBit(TIM5, TIM_IT_Update);
+  while(TIM_GetITStatus(TIM5, TIM_IT_Update) == RESET);
 }
 
 void pwm_init()
@@ -59,80 +97,86 @@ void pwm_init()
   GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;
   GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-	TIM_OCInitTypeDef TIM_OCStruct;
+  TIM_OCInitTypeDef TIM_OCStruct;
 
-	TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM1;
-	TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_High;
-	TIM_OCStruct.TIM_Pulse = 0;
+  TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCStruct.TIM_Pulse = 0;
 
-	TIM_OC1Init(TIM3, &TIM_OCStruct);
-	TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+  TIM_OC1Init(TIM3, &TIM_OCStruct);
+  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
-	TIM_OC2Init(TIM3, &TIM_OCStruct);
-	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+  TIM_OC2Init(TIM3, &TIM_OCStruct);
+  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
-	TIM_OC3Init(TIM3, &TIM_OCStruct);
-	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+  TIM_OC3Init(TIM3, &TIM_OCStruct);
+  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
-	TIM_OC4Init(TIM3, &TIM_OCStruct);
-	TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-
-	/* Code for verifying that the output of TIM2 is correct 
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_TIM2);
-  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
-  GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	TIM_OCInitTypeDef TIM2_OCStruct;
-
-	TIM2_OCStruct.TIM_OCMode = TIM_OCMode_PWM1;
-	TIM2_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
-	TIM2_OCStruct.TIM_OCPolarity = TIM_OCPolarity_High;
-	TIM2_OCStruct.TIM_Pulse = 500;
-
-	TIM_OC1Init(TIM2, &TIM2_OCStruct);
-	TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
-	*/
+  TIM_OC4Init(TIM3, &TIM_OCStruct);
+  TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
 }
 
 void set_pwm(int channelNumber, float pulsePercent)
 {
-	if (pulsePercent > 100.0f)
-		pulsePercent = 100.0f;
-	else if (pulsePercent < 0.0f)
-		pulsePercent = 0.0f;
+  if (pulsePercent > 100.0f)
+    pulsePercent = 100.0f;
+  else if (pulsePercent < 0.0f)
+    pulsePercent = 0.0f;
 
-	TIM_OCInitTypeDef TIM_OCStruct;
+  TIM_OCInitTypeDef TIM_OCStruct;
 
-	TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM1;
-	TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_High;
-	TIM_OCStruct.TIM_Pulse = (pulsePercent / 100.0f)*(1000.0f - 1.0f) - 1;
+  TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCStruct.TIM_Pulse = (pulsePercent / 100.0f)*(1000.0f - 1.0f) - 1;
 
-	switch (channelNumber)
-	{
-	case 1:
-		TIM_OC1Init(TIM3, &TIM_OCStruct);
-		TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
-		break;
+  switch (channelNumber)
+  {
+    case 1:
+      TIM_OC1Init(TIM3, &TIM_OCStruct);
+      TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+      break;
 
-	case 2:
-		TIM_OC2Init(TIM3, &TIM_OCStruct);
-		TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
-		break;
+    case 2:
+      TIM_OC2Init(TIM3, &TIM_OCStruct);
+      TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+      break;
 
-	case 3:
-		TIM_OC3Init(TIM3, &TIM_OCStruct);
-		TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
-		break;
+    case 3:
+      TIM_OC3Init(TIM3, &TIM_OCStruct);
+      TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+      break;
 
-	case 4:
-		TIM_OC4Init(TIM3, &TIM_OCStruct);
-		TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-		break;
+    case 4:
+      TIM_OC4Init(TIM3, &TIM_OCStruct);
+      TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
+      break;
 
-	default:
-		break;
-	}
+    default:
+      break;
+  }
+}
+
+uint32_t gettime_ms() {
+  return TIM_GetCounter(TIM2) / 1000;
+}
+uint32_t gettime_us() {
+  return TIM_GetCounter(TIM2);
+}
+
+void sleep_ms(uint32_t ms) {
+  sleep_us(ms*1000);
+}
+
+void sleep_us(uint32_t us) {
+  TIM4->CNT = 0;
+  while(TIM4->CNT < us);
+}
+
+void sleep_til_ms(uint32_t ms) {
+  return sleep_til_us(ms * 1000);
+}
+
+void sleep_til_us(uint32_t end) {
 }
--- software/src/tim.h
+++ software/src/tim.h
@@ -7,4 +7,16 @@ void tim_init();
 void pwm_init();
 void set_pwm(int channelNumber, float pulsePercent);
 
+uint32_t gettime_ms();
+uint32_t gettime_us();
+
+void sleep_ms(uint32_t ms);
+void sleep_us(uint32_t us);
+
+void sleep_til_us(uint32_t us);
+void sleep_til_ms(uint32_t ms);
+
+void periodic_init(int hz);
+void periodic_sleep();
+
 #endif // TIM_H
